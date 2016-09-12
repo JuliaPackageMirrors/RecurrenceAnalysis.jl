de9a4dec567a174c91f518b65b0a56949e8a9db7
heliosdrm <helios.derosario@gmail.com>
2016-09-12 18:39:43 +0200

    Windowed functions based on macro

--- src/windowed.jl
+++ src/windowed.jl
@@ -1,74 +1,110 @@
-# Diagonal histograms
-function dhist_windowed(x::AbstractMatrix{Bool}, winsize::Integer; kwargs...)
-    xw = x[1:winsize,1:winsize]
-    n = minimum(size(x))
-    nw = n-winsize+1
-    dh, fh, lh = dhist_extended(xw; kwargs...)
-    # Iterate
-    for i=1:nw-1
-        # Update the number of diagonals
-        # Shift down on the basis of first line histogram
-        # Shift up on the basis of the last line histogram
-        dh .-= fh+lh
-        dh[1:end-1] += fh[2:end]
-        dh[2:end] += lh[1:end-1]
-        (lh[end] > 0) && push!(dh,lh[end])
-        (dh[end] <= 0) && pop!(dh)
-    end
-    dh
-end
+matrix_funs2 = [
+    :crossdistancematrix,
+    :crossrecurrencematrix,
+    :jointrecurrencematrix
+    ]
+    
+matrix_funs1 = Dict(
+    :recurrencematrix => :crossrecurrencematrix,
+    :distancematrix => :crossdistancematrix)
 
-function dhist_extended(x::AbstractMatrix{Bool}; theiler::Integer=1, kwargs...)
-    theiler < 0 && error("Theiler window length must be greater than 0")
-    bins = [0]
-    firstbins = [0] #W
-    lastbins = [0] #W
-    firstsingle = 0 #W
-    lastsingle = 0 #W
-    nbins = 1
-    current_diag = 0
-    m, n = size(x)
-    # Iterate over diagonals - excluding LOI and Theiler window
-    # If the matrix is symmetric, examine only the upper triangle
-    diag_collection = collect(theiler:n-2)
-    xsym = issym(x) # compat!
-    !xsym && prepend!(diag_collection, collect(-(m-2):-max(theiler,1)))
-    for d in diag_collection
-        increment = (xsym && d > 0) ? 2 : 1
-        previous_cell = false
-        firstdiag = true #W
-        first_c = max(1, d+1)
-        last_c = min(n, m+d)
-        for c = first_c:last_c
-            # Operate on existing diagonal line
-            if previous_cell
-                # Extend length with current cell
-                (extend = x[c-d,c]) && (current_diag += 1)
-                # If arrived to the end of a line
-                # add the current length to the corresponding bin
-                if (!extend || (c == last_c)) && (current_diag > 0)
-                    # Append new positions to the bins if needed
-                    if current_diag > nbins
-                        append!(bins, zeros(current_diag - nbins))
-                        append!(firstbins, zeros(current_diag - nbins)) #W
-                        append!(lastbins, zeros(current_diag - nbins)) #W
-                        nbins = current_diag
-                    end
-                    bins[current_diag] += increment
-                    # Store value of last or first diag if it is the case #W
-                    (c == last_c) && extend && (lastbins[current_diag] += increment) #W
-                    firstdiag && (firstbins[current_diag] += increment) #W
-                    current_diag = 0
+rqa_funs = [
+    :recurrencerate,
+    :determinism,
+    :avgdiag,
+    :maxdiag,
+    :divergence,
+    :entropy,
+    :trend,
+    :laminarity,
+    :trappingtime,
+    :maxvert
+    ]
+
+macro window(ws, ex)
+    # Expression can be of type a = f(x...)
+    ret_ex = ex
+    if ex.head == :(=)
+        left, right = (ex.args...)
+        return :($(esc(left)) = @window $ws $right)
+    end
+    if ex.head == :call
+        f = ex.args[1]
+        x = ex.args[2]
+        # Iteration of RQA functions
+        fi = findfirst(rqa_funs, f)
+        if fi > 0
+            submat = :($x[i+w,i+w])
+            ex.args[2] = submat
+            ret_ex = quote
+                w = 1:$ws
+                nw = size($x,1) - $ws
+                [$ex for i=0:nw]
+            end
+            return ret_ex
+        end
+        if haskey(matrix_funs1, f)
+            ex.args[1] = matrix_funs1[f]
+            insert!(ex.args, 2, x)
+            return :(@window $ws $ex)
+        end
+        # Iteration of matrix construction (2 inputs)
+        fi = findfirst(matrix_funs2, f)
+        if fi > 0
+            y = ex.args[3]
+            ret_ex = Array(Expr,6)
+            # Prologue
+            ret_ex[1] = quote
+                n = size($x,1)
+                m = size($y,1)
+                mn = min(m,n)
+                retmat = map(Bool,spzeros(m,n))
+                w = 1:$ws
+                max_i = div(mn, $ws)
+            end
+            # Main diagonal
+            ex.args[2] = :($x[i*($ws)+w,:])
+            ex.args[3] = :($y[i*($ws)+w,:])
+            ret_ex[2] = quote
+                for i=0:max_i-1
+                    retmat[i*($ws)+w,i*($ws)+w] = $ex
                 end
             end
-            # Store value of previous cell: stop first diagonal search when reaching false
-            (previous_cell = x[c-d,c]) || (firstdiag=false) #W
+            ex.args[2] = :($x[max_i*($ws)+1:m,:])
+            ex.args[3] = :($y[max_i*($ws)+1:n,:])
+            ret_ex[3] = quote
+                retmat[max_i*($ws)+1:m,max_i*($ws)+1:n] = $ex
+            end
+            # Upper diagonal
+            ex.args[2] = :($x[(i-1)*($ws)+w,:])
+            ex.args[3] = :($y[i*($ws)+w,:])
+            ret_ex[4] = quote
+                for i=1:max_i-1
+                    retmat[(i-1)*$ws+w,i*$ws+w] = $ex
+                end
+            end
+            ex.args[2] = :($x[(max_i-1)*($ws)+w,:])
+            ex.args[3] = :($y[max_i*($ws)+1:n,:])
+            ret_ex[5] = quote
+                retmat[(max_i-1)*($ws)+w,max_i*($ws)+1:n] = $ex
+            end
+            # Lower diagonal
+            ex.args[2] = :($x[i*($ws)+w,:])
+            ex.args[3] = :($y[(i-1)*($ws)+w,:])
+            ret_ex[6] = quote
+                for i=1:max_i-1
+                    retmat[i*$ws+w,(i-1)*$ws+w] = $ex
+                end
+            end
+            ex.args[2] = :($x[max_i*($ws)+1:m,:])
+            ex.args[3] = :($y[(max_i-1)*($ws)+w,:])
+            ret_ex[5] = quote
+                retmat[max_i*($ws)+1:m,(max_i-1)*($ws)+w] = $ex
+            end
         end
-        # Isolated points #W
-        (x[first_c-d,first_c] && !x[first_c+1-d,first_c+1]) && (firstsingle += increment) #W
-        (!x[last_c-1-d,last_c-1] && x[last_c-d,last_c]) && (lastsingle += increment) #W
+        Throw error if it is not a valid function
+        error("$(string(ex.args[1])) is not a valid function for windowing")
     end
-    # Add isolated points in first bin
-    allpoints = (theiler == 0) ? countnz(x) : countnz(triu(x, theiler)) + countnz(tril(x,-theiler))
-    [allpoints - collect(2:nbins+1)'*bins; bins], [firstsingle; firstbins], [lastsingle; lastbins] #W
+    # Throw error if it didn't return
+    error("Invalid expression for windowing")
 end
