c6acec7c4a309c4dcd7968ab34d7755d4f38e541
Helios De Rosario <helios.derosario@gmail.com>
2016-09-12 23:58:58 +0200

    Extend window macro for matrices

--- src/windowed.jl
+++ src/windowed.jl
@@ -1,13 +1,3 @@
-matrix_funs2 = [
-    :crossdistancematrix,
-    :crossrecurrencematrix,
-    :jointrecurrencematrix
-    ]
-    
-matrix_funs1 = Dict(
-    :recurrencematrix => :crossrecurrencematrix,
-    :distancematrix => :crossdistancematrix)
-
 rqa_funs = [
     :recurrencerate,
     :determinism,
@@ -21,6 +11,29 @@ rqa_funs = [
     :maxvert
     ]
 
+function blockrecurrencematrix(x, y, bsize, dindex, vargs...; kwargs...)
+    n = min(size(x,1), size(y,1))
+    rmat = map(Bool, spzeros(n,n))
+    brange = 1:bsize
+    n_fullblocks = div(n, bsize)
+    for i=abs(dindex):n_fullblocks-1
+        ix = i*bsize+brange
+        iy = i*bsize+brange
+        if dindex < 0
+            ix += -dindex*bsize
+        elseif dindex > 0
+            iy += dindex*bsize
+        end
+        rmat[ix,iy] = crossrecurrencematrix(x[ix,:], y[iy,:], vargs...; kwargs...)
+    end
+    ix1 = ix[end]+1
+    iy1 = iy[end]+1
+    ix2 = min(ix1+bsize-1, n)
+    iy2 = min(iy1+bsize-1, n)
+    rmat[ix1:ix2, iy1:iy2] = crossrecurrencematrix(x[ix1:ix2,:], y[iy1:iy2,:], vargs...; kwargs...)
+    rmat
+end
+
 macro window(ws, ex)
     # Expression can be of type a = f(x...)
     ret_ex = ex
@@ -30,77 +43,25 @@ macro window(ws, ex)
     end
     if ex.head == :call
         f = ex.args[1]
-        x = ex.args[2]
-        # Iteration of RQA functions
-        fi = findfirst(rqa_funs, f)
-        if fi > 0
-            submat = :($x[i+w,i+w])
-            ex.args[2] = submat
-            ret_ex = quote
-                w = 1:$ws
-                nw = size($x,1) - $ws
-                [$ex for i=0:nw]
-            end
-            return ret_ex
-        end
-        if haskey(matrix_funs1, f)
-            ex.args[1] = matrix_funs1[f]
-            insert!(ex.args, 2, x)
-            return :(@window $ws $ex)
-        end
-        # Iteration of matrix construction (2 inputs)
-        fi = findfirst(matrix_funs2, f)
-        if fi > 0
-            y = ex.args[3]
-            ret_ex = Array(Expr,6)
-            # Prologue
-            ret_ex[1] = quote
-                n = size($x,1)
-                m = size($y,1)
-                mn = min(m,n)
-                retmat = map(Bool,spzeros(m,n))
-                w = 1:$ws
-                max_i = div(mn, $ws)
-            end
-            # Main diagonal
-            ex.args[2] = :($x[i*($ws)+w,:])
-            ex.args[3] = :($y[i*($ws)+w,:])
-            ret_ex[2] = quote
-                for i=0:max_i-1
-                    retmat[i*($ws)+w,i*($ws)+w] = $ex
-                end
-            end
-            ex.args[2] = :($x[max_i*($ws)+1:m,:])
-            ex.args[3] = :($y[max_i*($ws)+1:n,:])
-            ret_ex[3] = quote
-                retmat[max_i*($ws)+1:m,max_i*($ws)+1:n] = $ex
-            end
-            # Upper diagonal
-            ex.args[2] = :($x[(i-1)*($ws)+w,:])
-            ex.args[3] = :($y[i*($ws)+w,:])
-            ret_ex[4] = quote
-                for i=1:max_i-1
-                    retmat[(i-1)*$ws+w,i*$ws+w] = $ex
-                end
-            end
-            ex.args[2] = :($x[(max_i-1)*($ws)+w,:])
-            ex.args[3] = :($y[max_i*($ws)+1:n,:])
-            ret_ex[5] = quote
-                retmat[(max_i-1)*($ws)+w,max_i*($ws)+1:n] = $ex
-            end
-            # Lower diagonal
-            ex.args[2] = :($x[i*($ws)+w,:])
-            ex.args[3] = :($y[(i-1)*($ws)+w,:])
-            ret_ex[6] = quote
-                for i=1:max_i-1
-                    retmat[i*$ws+w,(i-1)*$ws+w] = $ex
-                end
-            end
-            ex.args[2] = :($x[max_i*($ws)+1:m,:])
-            ex.args[3] = :($y[(max_i-1)*($ws)+w,:])
-            ret_ex[5] = quote
-                retmat[max_i*($ws)+1:m,(max_i-1)*($ws)+w] = $ex
-            end
+        if f == :crossrecurrencematrix
+            ex.args[1] = :blockrecurrencematrix
+            insert!(ex.args, 4, ws)
+            insert!(ex.args, 5, -1)
+            exd_lower  = :(bl = $ex)
+            ex.args[5] = 0
+            exd_center = :(bd = $ex)
+            ex.args[5] = 1
+            exd_upper  = :(bu = $ex)
+            ex_ret = :(rmat = bl|bd|bu)
+        elseif f == :recurrencematrix
+            ex.args[1] = :blockrecurrencematrix
+            insert!(ex.args, 3, ex.args[2])
+            insert!(ex.args, 4, ws)
+            insert!(ex.args, 5, -1)
+            exd_lower  = :(bl = $ex)
+            ex.args[5] = 0
+            exd_center = :(bd = $ex)
+            ex_ret = :(rmat = bl|bd|bl')
         end
         Throw error if it is not a valid function
         error("$(string(ex.args[1])) is not a valid function for windowing")
