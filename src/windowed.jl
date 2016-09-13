06ec8ec4262eaaf085cdf00bebb4ebb37a08125d
Helios De Rosario <helios.derosario@gmail.com>
2016-09-13 16:41:44 +0200

    Complete macro for matrices
    Lacks @window jointrecurrencematrix and steps for RQA

--- src/windowed.jl
+++ src/windowed.jl
@@ -16,13 +16,14 @@ function blockrecurrencematrix(x, y, bsize, dindex, vargs...; kwargs...)
     rmat = map(Bool, spzeros(n,n))
     brange = 1:bsize
     n_fullblocks = div(n, bsize)
+    ix = iy = 0:0
     for i=abs(dindex):n_fullblocks-1
         ix = i*bsize+brange
         iy = i*bsize+brange
         if dindex < 0
-            ix += -dindex*bsize
+            iy -= -dindex*bsize
         elseif dindex > 0
-            iy += dindex*bsize
+            ix -= dindex*bsize
         end
         rmat[ix,iy] = crossrecurrencematrix(x[ix,:], y[iy,:], vargs...; kwargs...)
     end
@@ -36,34 +37,59 @@ end
 
 macro window(ws, ex)
     # Expression can be of type a = f(x...)
-    ret_ex = ex
     if ex.head == :(=)
         left, right = (ex.args...)
         return :($(esc(left)) = @window $ws $right)
     end
     if ex.head == :call
         f = ex.args[1]
+        # Iteration of RQA functions
+        fi = findfirst(rqa_funs, f)
+        if fi > 0
+            x = ex.args[2]
+            submat = :($x[i+w,i+w])
+            ex.args[2] = submat
+            ret_ex = quote
+                w = 1:$ws
+                nw = size($x,1) - $ws
+                [$ex for i=0:nw]
+            end
+            return ret_ex
+        end
         if f == :crossrecurrencematrix
             ex.args[1] = :blockrecurrencematrix
             insert!(ex.args, 4, ws)
             insert!(ex.args, 5, -1)
-            exd_lower  = :(bl = $ex)
+            exd_lower  = :(bl = $(parse(string(ex))))
             ex.args[5] = 0
-            exd_center = :(bd = $ex)
+            exd_center = :(bd = $(parse(string(ex))))
             ex.args[5] = 1
-            exd_upper  = :(bu = $ex)
-            ex_ret = :(rmat = bl|bd|bu)
+            exd_upper  = :(bu = $(parse(string(ex))))
+            exd_comp = :(bl|bd|bu)
+            ret_ex = quote
+                $exd_lower
+                $exd_center
+                $exd_upper
+                $exd_comp
+            end
+            return ret_ex
         elseif f == :recurrencematrix
             ex.args[1] = :blockrecurrencematrix
             insert!(ex.args, 3, ex.args[2])
             insert!(ex.args, 4, ws)
             insert!(ex.args, 5, -1)
-            exd_lower  = :(bl = $ex)
+            exd_lower  = :(bl = $(parse(string(ex))))
             ex.args[5] = 0
-            exd_center = :(bd = $ex)
-            ex_ret = :(rmat = bl|bd|bl')
+            exd_center = :(bd = $(parse(string(ex))))
+            exd_comp = :(bl|bd|bl')
+            ret_ex = quote
+                $exd_lower
+                $exd_center
+                $exd_comp
+            end
+            return ret_ex
         end
-        Throw error if it is not a valid function
+        # Throw error if it is not a valid function
         error("$(string(ex.args[1])) is not a valid function for windowing")
     end
     # Throw error if it didn't return
